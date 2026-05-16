2934adc9af72b9da00e5bda0a95004d3fdea0d0b
tvboelens <tvboelens@gmail.com>
2026-05-16 18:58:21 +0200

    1. Implementation of sending tcp response 2. Remover retries when sending tcp response

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,10 @@ pub mod tcp_protocol;
 use std::io;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use tcp_protocol::{TcpRequest, recv_tcp_request};
+use tcp_protocol::{TcpRequest, TcpResponse, recv_tcp_request};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::oneshot;
-use tokio::time::{Duration, sleep};
 use uuid::Uuid;
 
 use crate::storage_engine::{StorageEngine, WriteData, WriteJob};
@@ -24,7 +23,7 @@ struct PutData {
     key: String,
     value: String,
 }
-enum Request {
+enum Operation {
     Get(String),
     Put(PutData),
     Delete(String),
@@ -47,15 +46,16 @@ async fn handle_tcp_request(
     tcp_request: TcpRequest,
     storage_engine: Arc<StorageEngine>,
 ) -> Result<Option<String>, ServerError> {
-    Ok(None)
+    let op = Operation::from(tcp_request);
+    Ok(call_storage_engine(op, storage_engine).await?)
 }
 
 async fn call_storage_engine(
-    request: Request,
+    op: Operation,
     storage_engine: Arc<StorageEngine>,
 ) -> Result<Option<String>, StorageEngineError> {
-    match request {
-        Request::Delete(key) => {
+    match op {
+        Operation::Delete(key) => {
             let (tx, rx) = oneshot::channel();
             let job = WriteJob {
                 data: WriteData::Delete(key),
@@ -69,11 +69,11 @@ async fn call_storage_engine(
                 Err(_) => Err(StorageEngineError::Shutdown),
             }
         }
-        Request::Get(key) => match storage_engine.get(&key) {
+        Operation::Get(key) => match storage_engine.get(&key) {
             Some(value) => Ok(Some(value)),
             None => Err(StorageEngineError::NotFound),
         },
-        Request::Put(data) => {
+        Operation::Put(data) => {
             let (tx, rx) = oneshot::channel();
             let job = WriteJob {
                 data: WriteData::Put(storage_engine::PutData {
@@ -93,22 +93,14 @@ async fn call_storage_engine(
     }
 }
 
-async fn send_response(
-    socket: &mut TcpStream,
-    value: &Option<String>,
-    correlation_id: &Uuid,
-) -> io::Result<()> {
-    let bytes = Vec::<u8>::new();
-    socket.write_all(&bytes).await
+async fn send_response(socket: &mut TcpStream, value: Option<String>, correlation_id: &Uuid) {
+    let bytes = TcpResponse::from(correlation_id, value).to_bytes();
+    socket.write_all(&bytes).await.unwrap_or(())
 }
 
-async fn send_error_response(
-    socket: &mut TcpStream,
-    error: &ServerError,
-    correlation_id: &Uuid,
-) -> io::Result<()> {
-    let bytes = Vec::<u8>::new();
-    socket.write_all(&bytes).await
+async fn send_error_response(socket: &mut TcpStream, error: &ServerError, correlation_id: &Uuid) {
+    let bytes = TcpResponse::from_error(correlation_id, error).to_bytes();
+    socket.write_all(&bytes).await.unwrap_or(())
 }
 
 async fn process_socket(mut socket: TcpStream, storage_engine_ptr: Arc<StorageEngine>) {
@@ -116,50 +108,23 @@ async fn process_socket(mut socket: TcpStream, storage_engine_ptr: Arc<StorageEn
         Ok(request) => {
             let correlation_id = request.headers.correlation_id;
             match handle_tcp_request(request, storage_engine_ptr).await {
-                Ok(res) => {
-                    let mut retry_count = 0;
-                    while let Err(_) = send_response(&mut socket, &res, &correlation_id).await {
-                        if retry_count > RESPONSE_RETRY_COUNT {
-                            break;
-                        }
-                        retry_count += 1;
-                        sleep(Duration::from_millis(50)); // TODO: log failure
-                    }
-                }
-                Err(e) => {
-                    let mut retry_count = 0;
-                    while let Err(_) = send_error_response(&mut socket, &e, &correlation_id).await {
-                        if retry_count > RESPONSE_RETRY_COUNT {
-                            break;
-                        }
-                        retry_count += 1;
-                        sleep(Duration::from_millis(50)); // TODO: log failure
-                    }
-                }
+                Ok(res) => send_response(&mut socket, res, &correlation_id).await,
+                Err(e) => send_error_response(&mut socket, &e, &correlation_id).await,
             }
         }
-        Err(e) => {
-            match e {
-                tcp_protocol::TcpError::IoError(_) | tcp_protocol::TcpError::WrongMagicBytes => (),
-                tcp_protocol::TcpError::InvalidKey(id)
-                | tcp_protocol::TcpError::InvalidRequestType(id)
-                | tcp_protocol::TcpError::InvalidValue(id)
-                | tcp_protocol::TcpError::MalformedPayload(id)
-                | tcp_protocol::TcpError::MissingValue(id)
-                | tcp_protocol::TcpError::UnknownFlags(id)
-                | tcp_protocol::TcpError::UnsupportedVersion(id) => {
-                    let error = ServerError::TcpError(e);
-                    let mut retry_count = 0;
-                    while let Err(_) = send_error_response(&mut socket, &error, &id).await {
-                        if retry_count > RESPONSE_RETRY_COUNT {
-                            break;
-                        }
-                        retry_count += 1;
-                        sleep(Duration::from_millis(50)); // TODO: log failure
-                    }
-                }
+        Err(e) => match e {
+            tcp_protocol::TcpError::IoError(_) | tcp_protocol::TcpError::WrongMagicBytes => (),
+            tcp_protocol::TcpError::InvalidKey(id)
+            | tcp_protocol::TcpError::InvalidRequestType(id)
+            | tcp_protocol::TcpError::InvalidValue(id)
+            | tcp_protocol::TcpError::MalformedPayload(id)
+            | tcp_protocol::TcpError::MissingValue(id)
+            | tcp_protocol::TcpError::UnknownFlags(id)
+            | tcp_protocol::TcpError::UnsupportedVersion(id) => {
+                let error = ServerError::TcpError(e);
+                send_error_response(&mut socket, &error, &id).await
             }
-        } // Log error?
+        }, // Log error?
     }
 }
 
@@ -187,7 +152,7 @@ impl From<tcp_protocol::TcpError> for ServerError {
     }
 }
 
-impl From<tcp_protocol::TcpRequest> for Request {
+impl From<TcpRequest> for Operation {
     fn from(request: tcp_protocol::TcpRequest) -> Self {
         match request.headers.request_type {
             tcp_protocol::RequestType::Delete => Self::Delete(request.payload.key),
