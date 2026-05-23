bfca1facc852eac23027854eda6a1ded8ed3b4f8
tvboelens <tvboelens@gmail.com>
2026-05-23 16:15:53 +0200

    Make API of storage engine async so that we can remove atomic ptr to memtable

--- src/storage_engine.rs
+++ src/storage_engine.rs
@@ -1,43 +1,49 @@
 use std::io;
-use std::sync::atomic::{AtomicPtr, Ordering};
+use std::sync::mpsc;
 use std::sync::mpsc::{RecvTimeoutError, SendError};
-use std::sync::{Arc, mpsc};
 use std::time::{Duration, Instant};
-use tokio::sync::oneshot::Sender;
+use tokio::sync::oneshot::{Sender, channel};
 
 pub mod memtable;
 pub mod wal;
 
 pub trait Store {
-    fn get(&self, key: &String) -> Option<String>;
-    fn submit_put(&self, job: WriteJob) -> Result<(), SendError<WriteJob>>;
-    fn submit_delete(&self, job: WriteJob) -> Result<(), SendError<WriteJob>>;
+    fn get(
+        &self,
+        key: &String,
+    ) -> impl std::future::Future<Output = Result<Option<String>, StorageEngineError>> + Send;
+    fn put(
+        &self,
+        key: &String,
+        value: &String,
+    ) -> impl std::future::Future<Output = Result<Option<String>, StorageEngineError>> + Send;
+    fn delete(
+        &self,
+        key: &String,
+    ) -> impl std::future::Future<Output = Result<Option<String>, StorageEngineError>> + Send;
 }
 
-pub struct PutData {
-    pub key: String,
-    pub value: String,
-}
-pub enum WriteData {
+enum Command {
     Delete(String),
-    Put(PutData),
+    Get(String),
+    Put(String, String),
 }
 
-pub struct WriteJob {
-    pub data: WriteData,
+struct Job {
+    pub command: Command,
     pub sender: Sender<io::Result<Option<String>>>,
 }
 
 pub struct StorageEngine {
-    memtable: Arc<AtomicPtr<memtable::MemTable>>,
-    sender: mpsc::Sender<WriteJob>,
+    //memtable: Arc<AtomicPtr<memtable::MemTable>>,
+    sender: mpsc::Sender<Job>,
     join_handle: std::thread::JoinHandle<()>,
 }
 
 struct Worker {
-    memtable: Arc<AtomicPtr<memtable::MemTable>>,
+    memtable: memtable::MemTable,
     wal: wal::Wal,
-    receiver: mpsc::Receiver<WriteJob>,
+    receiver: mpsc::Receiver<Job>,
     timeout: Duration,
 }
 
@@ -45,41 +51,76 @@ pub struct StorageEngineConf {
     timeout: Duration,
 }
 
+#[derive(Debug)]
+pub enum StorageEngineError {
+    IoError,
+    NotFound,
+    Shutdown,
+}
+
 impl Store for StorageEngine {
-    fn get(&self, key: &String) -> Option<String> {
-        /* This will have to get more sophisticated once we move to on-disk persistence
-           Also the unwrap should be replaced later by something that returns None
-           so that the server can return a not found error or similar
-        */
-        let memtable: &memtable::MemTable = unsafe { &*self.memtable.load(Ordering::Acquire) };
-        memtable.get(key)
+    async fn get(&self, key: &String) -> Result<Option<String>, StorageEngineError> {
+        let (tx, rx) = channel();
+        let cmd = Command::Get(key.clone());
+        let job = Job {
+            command: cmd,
+            sender: tx,
+        };
+        self.sender.send(job)?;
+        match rx.await {
+            Ok(res) => res.map_err(|_| StorageEngineError::IoError),
+            Err(_) => Err(StorageEngineError::Shutdown),
+        }
     }
 
-    fn submit_put(&self, job: WriteJob) -> Result<(), SendError<WriteJob>> {
-        self.sender.send(job)
+    async fn put(
+        &self,
+        key: &String,
+        value: &String,
+    ) -> Result<Option<String>, StorageEngineError> {
+        let (tx, rx) = channel();
+        let cmd = Command::Put(key.clone(), value.clone());
+        let job = Job {
+            command: cmd,
+            sender: tx,
+        };
+        self.sender.send(job)?;
+        match rx.await {
+            Ok(res) => res.map_err(|_| StorageEngineError::IoError),
+            Err(_) => Err(StorageEngineError::Shutdown),
+        }
     }
 
-    fn submit_delete(&self, job: WriteJob) -> Result<(), SendError<WriteJob>> {
-        self.sender.send(job)
+    async fn delete(&self, key: &String) -> Result<Option<String>, StorageEngineError> {
+        let (tx, rx) = channel();
+        let cmd = Command::Delete(key.clone());
+        let job = Job {
+            command: cmd,
+            sender: tx,
+        };
+        self.sender.send(job)?;
+        match rx.await {
+            Ok(res) => res.map_err(|_| StorageEngineError::IoError),
+            Err(_) => Err(StorageEngineError::Shutdown),
+        }
     }
 }
 
 impl StorageEngine {
     pub fn new(config: StorageEngineConf) -> Self {
-        let mut memtable = memtable::MemTable::new();
-        let memtable_ptr = Arc::new(AtomicPtr::new(&mut memtable));
+        let memtable = memtable::MemTable::new();
         let wal = wal::Wal::from(0);
         let (tx, rx) = mpsc::channel();
 
         let mut worker = Worker {
-            memtable: memtable_ptr.clone(),
+            memtable: memtable,
             wal: wal,
             receiver: rx,
             timeout: config.timeout,
         };
         let handle = std::thread::spawn(move || worker.run());
         StorageEngine {
-            memtable: memtable_ptr.clone(),
+            //memtable: memtable_ptr.clone(),
             sender: tx,
             join_handle: handle,
         }
@@ -106,22 +147,20 @@ impl Worker {
                     }
                 },
                 Ok(job) => {
-                    self.do_write(job);
+                    let res = match job.command {
+                        Command::Delete(key) => self.do_delete(key),
+                        Command::Get(key) => Ok(self.do_get(&key)),
+                        Command::Put(key, value) => self.do_put(&key, &value),
+                    };
                     elapsed -= Instant::now() - last_sync;
                     if elapsed.is_zero() {
                         self.wal.sync().unwrap();
                         last_sync = Instant::now();
                         elapsed = self.timeout;
                     }
+                    job.sender.send(res).unwrap_or(())
                 }
             }
-        }
-    }
-
-    fn do_write(&mut self, job: WriteJob) {
-        match job.data {
-            WriteData::Delete(key) => job.sender.send(self.do_delete(key)).unwrap_or(()),
-            WriteData::Put(data) => job.sender.send(self.do_put(data)).unwrap_or(()),
         }
     }
 
@@ -133,34 +172,27 @@ impl Worker {
             sequence_number: self.wal.last_sequence_number() + 1,
         };
         self.wal.append(&entry)?;
-        let mut memtable = unsafe { (*self.memtable.load(Ordering::Relaxed)).clone() };
-        match memtable.delete(&key) {
-            Some(value) => {
-                let _ = self.memtable.swap(&mut memtable, Ordering::Release);
-                Ok(Some(value))
-            }
-            None => Ok(None),
-        }
+        Ok(self.memtable.delete(&key))
     }
 
-    fn do_put(&mut self, data: PutData) -> Result<Option<String>, std::io::Error> {
+    fn do_put(&mut self, key: &String, value: &String) -> Result<Option<String>, std::io::Error> {
         let entry = wal::WalEntry {
             operation_type: wal::OpType::Put,
-            key: data.key.clone(),
-            value: data.value.clone(),
+            key: key.clone(),
+            value: value.clone(),
             sequence_number: self.wal.last_sequence_number() + 1,
         };
         self.wal.append(&entry)?;
-        let mut memtable = unsafe { (*self.memtable.load(Ordering::Relaxed)).clone() };
-        match memtable.put(data.key, data.value) {
-            Some(value) => {
-                let _ = self.memtable.swap(&mut memtable, Ordering::Release);
-                Ok(Some(value))
-            }
-            None => {
-                let _ = self.memtable.swap(&mut memtable, Ordering::Release);
-                Ok(None)
-            }
-        }
+        Ok(self.memtable.put(key.clone(), value.clone()))
+    }
+
+    fn do_get(&self, key: &String) -> Option<String> {
+        self.memtable.get(key)
+    }
+}
+
+impl From<SendError<Job>> for StorageEngineError {
+    fn from(_: SendError<Job>) -> Self {
+        StorageEngineError::Shutdown
     }
 }
